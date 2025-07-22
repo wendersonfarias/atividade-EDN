66a475fbcdbe557618d0a5f8b59757c38c164956
Wenderson Farias <wendersonpfarias@gmail.com>
2025-07-22 19:48:09 -0400

    atividade 3

---
+++ atividade3.py
@@ -0,0 +1,11 @@
+# 3- Calculadora de Volume
+# * Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+# * Comprimento: 12 cm
+# * Largura: 14 cm
+# * Altura: 20 cm
+# O programa deve calcular o volume e exibir o resultado em cm³.
+
+Comprimento = 12
+Largura = 14
+Altura = 20
